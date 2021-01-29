8682b869d8b76aef246f134e14238dce03995d62
toriecr <toriecr@gmail.com>
2021-01-29 11:57:48 -0800

    Add Item component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,98 @@
 import './App.css';
 import Header from "./components/Header"
+import Item from "./components/Item"
+
+import Accessories from "./assets/Desktop-Accessories.jpg"
+import ModelS from "./assets/Desktop-ModelS.jpeg"
+import Model3 from "./assets/Desktop-Model3.jpeg"
+import ModelX from "./assets/Desktop-ModelX.jpeg"
+import ModelY from "./assets/Desktop-ModelY.jpeg"
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg"
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg"
 
 function App() {
   return (
     <div className="App">
       <Header />
-      {/* header */}
-        {/* items => snapping */}
-        {/* items => snapping */}
-        {/* items => snapping */}
+      <div className="app__itemsContainer">
+        <Item 
+          title="Model S"
+          desc="Order online for Touchless Delivery"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="EXISTING INVENTORY"
+          rightBtnLink=""
+          twoButtons="true"
+          first="true"
+        />
+        <Item 
+          title="Model 3"
+          desc="Order online for Touchless Delivery"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="EXISTING INVENTORY"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item 
+          title="Model X"
+          desc="Order online for Touchless Delivery"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="EXISTING INVENTORY"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item 
+          title="Model Y"
+          desc="Order online for Touchless Delivery"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt="CUSTOM ORDER"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item 
+          title="Solar for New Roofs"
+          desc="Solar Roof Costs Less Than a New Roof Plus Solar Panels"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item 
+          title="Lowest Cost Solar Panels in America"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt="ORDER NOW"
+          leftBtnLink=""
+          rightBtnTxt="LEARN MORE"
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item 
+          title="Accessories"
+          desc=""
+          descLink=""
+          backgroundImg={Accessories}
+          leftBtnTxt="SHOP NOW"
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+        />
+      </div>
     </div>
   );
 }
